fd066507e319fbc12e8fe0bf3dab68d13219f6d1
hamza0426 <166389354+hamza0426@users.noreply.github.com>
2025-02-11 02:42:01 +0500

    Update Reservation.jsx

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -16,7 +16,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://mern-project-two-rho.vercel.app/reservation/send",
+        "https://mern-project-two-rho.vercel.app/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
